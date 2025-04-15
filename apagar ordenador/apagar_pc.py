a8871e9d151505f5916f86dc4af5854fd3f62662
Isabel RM <kaluca06@gmail.com>
2025-04-15 20:35:14 +0200

    feat: añadir mostrarHora()

--- apagar ordenador/apagar_pc.py
+++ apagar ordenador/apagar_pc.py
@@ -1,10 +1,11 @@
 
-## 
+##
 # Este programa presenta un menú con dos opciones; Hibernar o Apagar, una vez elegida la opción deseada, pregunta el tiempo en minutos pasado el cual queremos que ejecute la acción.
 #
 
 import os
 import time
+from datetime import datetime
 
 def apagar(minutos):
     segundos = minutos * 60
@@ -20,16 +21,24 @@ def hibernar(minutos):
         time.sleep(1)
     os.system("shutdown /h /f")
 
+    # Mostrar la hora actual
+def mostrarHora():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print(f"\nHora actual: {current_time}")
+
 while True:
     print("\n1. Apagar el ordenador")
     print("2. Hibernar el ordenador")
     opcion = input("\nSelecciona una opción: ")
     if opcion == "1":
         minutos = int(input("\n¿En cuántos minutos deseas apagar el ordenador?: "))
+        mostrarHora()
         apagar(minutos)
         break
     elif opcion == "2":
         minutos = int(input("\n¿En cuántos minutos deseas hibernar el ordenador?: "))
+        mostrarHora()
         hibernar(minutos)
         break
     else:
